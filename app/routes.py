19fdb2cd12f2e3c298705ab011bad36b32ff24c5
sotone <sotofabian227@gmail.com>
2023-02-06 17:21:51 -0500

    added the more info page and a main page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from app import app
-from flask import render_template,url_for
+from flask import render_template,url_for,redirect
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/seemore')
+def see_more():
+    return render_template('knowmore.html')
