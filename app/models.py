15f78cab4296f3d8bfbc95760130817631a2dd78
sotone <sotofabian227@gmail.com>
2023-02-07 17:11:38 -0500

    added login, signup, and a coursel on the homepage

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import UserMixin
+from app import db
+
+
+class User(db.model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(256), nullable=False)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.password = generate_password_hash(kwargs['password'])
+        db.session.add(self)
+        db.session.commit()
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,21 @@
 from app import app
-from flask import render_template,url_for,redirect
+from flask import render_template
+from app.forms import SignUpForm, LoginForm
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/seemore')
-def see_more():
-    return render_template('knowmore.html')
+@app.route('/contactus')
+def contactus():
+    return render_template('contactus.html')
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    form = SignUpForm()
+    return render_template('signup.html', form=form)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
